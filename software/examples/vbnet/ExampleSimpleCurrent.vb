d6d362f917a0593fb82748fa8d596ef960de8762
Matthias Bolte <matthias@tinkerforge.com>
2015-08-18 16:37:09 +0200

    Cleanup VB.NET examples

--- software/examples/vbnet/ExampleSimpleCurrent.vb
+++ software/examples/vbnet/ExampleSimpleCurrent.vb
@@ -1,23 +1,24 @@
 Imports Tinkerforge
 
-Module ExampleSimple
+Module ExampleSimpleCurrent
     Const HOST As String = "localhost"
     Const PORT As Integer = 4223
     Const UID As String = "XYZ" ' Change to your UID
 
     Sub Main()
         Dim ipcon As New IPConnection() ' Create IP connection
-        Dim ao As New BrickletIndustrialAnalogOut(UID, ipcon) ' Create device object
+        Dim iao As New BrickletIndustrialAnalogOut(UID, ipcon) ' Create device object
 
         ipcon.Connect(HOST, PORT) ' Connect to brickd
         ' Don't use device before ipcon is connected
 
-        'Set output current to 4.5mA
-        ao.SetCurrent(4500)
-        ao.Enable()
+        ' Set output current to 4.5mA
+        iao.SetCurrent(4500)
+        iao.Enable()
 
         System.Console.WriteLine("Press key to exit")
         System.Console.ReadLine()
+        iao.Disable()
         ipcon.Disconnect()
     End Sub
 End Module
