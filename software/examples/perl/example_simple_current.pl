0a1d6f866ff7c072fcd39b8091a9eafc8887d819
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:47:28 +0200

    Cleanup Perl examples

--- software/examples/perl/example_simple_current.pl
+++ software/examples/perl/example_simple_current.pl
@@ -8,16 +8,16 @@ use constant PORT => 4223;
 use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $ao = Tinkerforge::BrickletIndustrialAnalogOut->new(&UID, $ipcon); # Create device object
+my $iao = Tinkerforge::BrickletIndustrialAnalogOut->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Set output current to 4.5mA
-$ao->set_current(4500);
-$ao->enable();
+$iao->set_current(4500);
+$iao->enable();
 
 print "Press any key to exit...\n";
 <STDIN>;
+$iao->disable();
 $ipcon->disconnect();
-
--- software/examples/perl/example_simple_voltage.pl
+++ software/examples/perl/example_simple_voltage.pl
@@ -8,16 +8,16 @@ use constant PORT => 4223;
 use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $ao = Tinkerforge::BrickletIndustrialAnalogOut->new(&UID, $ipcon); # Create device object
+my $iao = Tinkerforge::BrickletIndustrialAnalogOut->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Set output voltage to 3.3V
-$ao->set_voltage(3300);
-$ao->enable();
+$iao->set_voltage(3300);
+$iao->enable();
 
 print "Press any key to exit...\n";
 <STDIN>;
+$iao->disable();
 $ipcon->disconnect();
-
