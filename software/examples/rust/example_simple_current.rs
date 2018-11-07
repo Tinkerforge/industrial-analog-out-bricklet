a5536ca43fa532da00ff8b1d6d453d90273a262c
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:21:41 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple_current.rs
+++ software/examples/rust/example_simple_current.rs
@@ -1,26 +1,26 @@
 use std::{error::Error, io};
 
-use tinkerforge::{industrial_analog_out_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_analog_out_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Analog Out Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Analog Out Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_analog_out_bricklet = IndustrialAnalogOutBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let iao = IndustrialAnalogOutBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Set output current to 4.5mA
-    industrial_analog_out_bricklet.set_current(4500);
-    industrial_analog_out_bricklet.enable();
+    iao.set_current(4500);
+    iao.enable();
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    industrial_analog_out_bricklet.disable();
+    iao.disable();
     ipcon.disconnect();
     Ok(())
 }
--- software/examples/rust/example_simple_voltage.rs
+++ software/examples/rust/example_simple_voltage.rs
@@ -1,26 +1,26 @@
 use std::{error::Error, io};
 
-use tinkerforge::{industrial_analog_out_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_analog_out_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Analog Out Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Analog Out Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_analog_out_bricklet = IndustrialAnalogOutBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let iao = IndustrialAnalogOutBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Set output voltage to 3.3V
-    industrial_analog_out_bricklet.set_voltage(3300);
-    industrial_analog_out_bricklet.enable();
+    iao.set_voltage(3300);
+    iao.enable();
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    industrial_analog_out_bricklet.disable();
+    iao.disable();
     ipcon.disconnect();
     Ok(())
 }
